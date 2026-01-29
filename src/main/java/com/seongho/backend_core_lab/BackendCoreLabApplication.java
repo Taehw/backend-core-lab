c475be4b05b5a6d4e2b508a90ac5d2586ae3ba62
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2026-01-29 21:45:04 +0900

    initial commit

---
+++ src/main/java/com/seongho/backend_core_lab/BackendCoreLabApplication.java
@@ -0,0 +1,13 @@
+package com.seongho.backend_core_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCoreLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCoreLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seongho/backend_core_lab/BackendCoreLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seongho.backend_core_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCoreLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
